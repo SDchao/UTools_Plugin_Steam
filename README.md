4fe2db2cbad02e44aa7fcd36cdd5f93398d16c3d
Zeeman Zhang <cn.zeeman.zhang@gmail.com>
2021-11-24 17:22:51 +0800

    code format

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # 简介
-一款基于UTools的插件，可以快速打开已经在Steam上下载完毕的游戏或应用。
+
+一款基于 UTools 的插件，可以快速打开已经在 Steam 上下载完毕的游戏或应用。
 
 # 使用说明
+
 通过 "Steam" 关键字触发命令
 
 触发后可搜索游戏应用
@@ -9,30 +11,36 @@
 # 更新日志
 
 ## 1.0.5
-修复找不到其他steamlibrary的情况
+
+修复找不到其他 steamlibrary 的情况
 
 感谢 **[ciaranchen](https://github.com/ciaranchen)**
 
 ## 1.0.4
+
 增加直接输入应用名称启动功能
 
-加入缓存清理，命令为 "Steam缓存清理"
+加入缓存清理，命令为 "Steam 缓存清理"
 
 ## 1.0.2
+
 修复不存在库文件夹时无法加载列表
 
 ## 1.0.1
+
 修正名称过滤后的排序
 
 # 系统需求
-仅在Windows系统生效。
 
-在重装系统后直接启动Steam或直接拷贝安装的Steam可能无法被检测到。
+仅在 Windows 系统生效。
+
+在重装系统后直接启动 Steam 或直接拷贝安装的 Steam 可能无法被检测到。
 
 要求在以下注册表路径有内容：
 
-* 32位系统：HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam
-* 64位系统：HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam
+- 32 位系统：HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam
+- 64 位系统：HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam
 
 # 问题反馈
-* [Github](https://github.com/SDchao/UTools_Plugin_Steam/issues/new)
+
+- [Github](https://github.com/SDchao/UTools_Plugin_Steam/issues/new)
--- preload.js
+++ preload.js
@@ -1,241 +1,240 @@
 const cp = require('child_process')
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-const REG64STEAMPATH = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Valve\\Steam"
-const REG32STEAMPATH = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Valve\\Steam"
+const REG64STEAMPATH = 'HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Valve\\Steam'
+const REG32STEAMPATH = 'HKEY_LOCAL_MACHINE\\SOFTWARE\\Valve\\Steam'
 
-const URL_STEAMRUN = "steam://rungameid/"
+const URL_STEAMRUN = 'steam://rungameid/'
 const IDBLACKLIST = ['228980']
 
 let appListCache = []
 
 function GetSteamPath() {
-    let command = "REG QUERY "
-    let regex = /^\s+InstallPath\s+REG_SZ\s+(.*)$/m
+  let command = 'REG QUERY '
+  let regex = /^\s+InstallPath\s+REG_SZ\s+(.*)$/m
 
-    let r = ""
-    let res = ""
+  let r = ''
+  let res = ''
 
-    // Try 64-bit Reg Query
-    try {
-        r = cp.execSync(command + REG64STEAMPATH).toString()
-        res = regex.exec(r)
-        if (res) {
-            return res[1];
-        }
+  // Try 64-bit Reg Query
+  try {
+    r = cp.execSync(command + REG64STEAMPATH).toString()
+    res = regex.exec(r)
+    if (res) {
+      return res[1]
     }
-    catch (err) {
-        // console.error(err);
-    }
+  } catch (err) {
+    // console.error(err);
+  }
 
-    // Try 32-bit Reg Query
-    try {
-        r = cp.execSync(command + REG32STEAMPATH).toString()
-        res = regex.exec(r)
-        if (res) {
-            return res[1];
-        }
+  // Try 32-bit Reg Query
+  try {
+    r = cp.execSync(command + REG32STEAMPATH).toString()
+    res = regex.exec(r)
+    if (res) {
+      return res[1]
     }
-    catch (err) {
-        // console.error(err)
-    }
+  } catch (err) {
+    // console.error(err)
+  }
 
-    // Cannot find steam
-    return null;
+  // Cannot find steam
+  return null
 }
 
 function GetSteamAppsList() {
-    let steamPath = GetSteamPath()
-    if (!steamPath) {
-        return [{ title: '无法找到Steam', description: 'Steam或许未被正常安装', url: '' }]
+  let steamPath = GetSteamPath()
+  if (!steamPath) {
+    return [
+      { title: '无法找到Steam', description: 'Steam或许未被正常安装', url: '' },
+    ]
+  } else {
+    let appList = []
+    let appidRegex = /"appid"\s+"([0-9]+)"$/m
+    let appnameRegex = /"name"\s+"(.+)"$/m
+
+    let steamAppsPathList = [steamPath + '\\steamapps']
+
+    // 查找其他的Library
+    let libraryRegex1 = /"path".+"(.+)"/g
+    let libraryRegex2 = /"\d+".+"(.+)"/g
+    let content = fs
+      .readFileSync(path.join(steamAppsPathList[0], 'libraryfolders.vdf'))
+      .toString()
+    try {
+      let res = libraryRegex1.exec(content)
+      while (res) {
+        let rawPath = res[1]
+        steamAppsPathList.push(rawPath.replace('\\\\', '\\') + '\\steamapps')
+        res = libraryRegex1.exec(content)
+      }
+
+      res = libraryRegex2.exec(content)
+      while (res) {
+        let rawPath = res[1]
+        steamAppsPathList.push(rawPath.replace('\\\\', '\\') + '\\steamapps')
+        res = libraryRegex2.exec(content)
+      }
+    } catch (err) {
+      console.error('Unable to solve library: ' + file)
     }
-    else {
-        let appList = []
-        let appidRegex = /"appid"\s+"([0-9]+)"$/m
-        let appnameRegex = /"name"\s+"(.+)"$/m
 
-        let steamAppsPathList = [steamPath + "\\steamapps"]
-
-        // 查找其他的Library
-        let libraryRegex1 = /"path".+"(.+)"/g
-        let libraryRegex2 = /"\d+".+"(.+)"/g
-        let content = fs.readFileSync(path.join(steamAppsPathList[0], "libraryfolders.vdf")).toString()
-        try {
-            let res = libraryRegex1.exec(content)
-            while (res) {
-                let rawPath = res[1]
-                steamAppsPathList.push(rawPath.replace("\\\\", "\\") + "\\steamapps")
-                res = libraryRegex1.exec(content)
-            }
-
-            res = libraryRegex2.exec(content)
-            while (res) {
-                let rawPath = res[1]
-                steamAppsPathList.push(rawPath.replace("\\\\", "\\") + "\\steamapps")
-                res = libraryRegex2.exec(content)
-            }
-        }
-        catch (err) {
-            console.error("Unable to solve library: " + file)
-        }
-
-        for (let i in steamAppsPathList) {
-            steamAppsPath = steamAppsPathList[i]
+    for (let i in steamAppsPathList) {
+      steamAppsPath = steamAppsPathList[i]
+      try {
+        let fileList = fs.readdirSync(steamAppsPath)
+        for (let j in fileList) {
+          file = fileList[j]
+          let extname = path.extname(file)
+          if (extname == '.acf') {
+            let content = fs
+              .readFileSync(path.join(steamAppsPath, file))
+              .toString()
             try {
-                let fileList = fs.readdirSync(steamAppsPath)
-                for (let j in fileList) {
-                    file = fileList[j]
-                    let extname = path.extname(file)
-                    if (extname == ".acf") {
-                        let content = fs.readFileSync(path.join(steamAppsPath, file)).toString()
-                        try {
-                            let appid = appidRegex.exec(content)[1]
+              let appid = appidRegex.exec(content)[1]
 
-                            // appid是否在黑名单
-                            if (IDBLACKLIST.includes(appid))
-                                continue;
+              // appid是否在黑名单
+              if (IDBLACKLIST.includes(appid)) continue
 
-                            let appname = appnameRegex.exec(content)[1]
-                            appList.push({ title: appname, description: appid, url: URL_STEAMRUN + appid, lowtitle: appname.toLowerCase() })
-                        }
-                        catch (err) {
-                            console.error("Unable to solve game:" + file)
-                        }
-                    }
-                }
+              let appname = appnameRegex.exec(content)[1]
+              appList.push({
+                title: appname,
+                description: appid,
+                url: URL_STEAMRUN + appid,
+                lowtitle: appname.toLowerCase(),
+              })
+            } catch (err) {
+              console.error('Unable to solve game:' + file)
             }
-            catch (err) {
-                console.error("Unable to solve library: " + steamAppsPath)
-            }
+          }
         }
-        return appList;
+      } catch (err) {
+        console.error('Unable to solve library: ' + steamAppsPath)
+      }
     }
+    return appList
+  }
 }
 
 function CacheCheck() {
-    // 无缓冲列表 或者 第一个url为空
-    if (appListCache.length == 0 || !appListCache[0].url) {
-        appListCache = GetSteamAppsList()
-    }
+  // 无缓冲列表 或者 第一个url为空
+  if (appListCache.length == 0 || !appListCache[0].url) {
+    appListCache = GetSteamAppsList()
+  }
 }
 
 function GetFilteredAppsList(word) {
+  if (!word) return appListCache
 
-    if (!word) return appListCache
-
-    word = word.toLowerCase()
-    let resultList = []
-    for (let j in appListCache) {
-        e = appListCache[j]
-        let lastIndex = -1
-        let lowtitle = e.lowtitle
-        let charList = word.split("")
-        let isMatch = true
-        let matchScore = 0  // Higher score stands for lower relevant
-        for (let i in charList) {
-            char = charList[i]
-            let nowIndex = lowtitle.indexOf(char, lastIndex + 1)
-            if (nowIndex == -1) {
-                isMatch = false
-                break
-            }
-            else {
-                if (nowIndex != 0) {
-                    let prevChar = lowtitle[nowIndex - 1]
-                    let alphaRegex = /^[a-z0-9]$/
-                    if (alphaRegex.test(prevChar))
-                        matchScore += nowIndex - lastIndex
-                }
-                lastIndex = nowIndex
-            }
+  word = word.toLowerCase()
+  let resultList = []
+  for (let j in appListCache) {
+    e = appListCache[j]
+    let lastIndex = -1
+    let lowtitle = e.lowtitle
+    let charList = word.split('')
+    let isMatch = true
+    let matchScore = 0 // Higher score stands for lower relevant
+    for (let i in charList) {
+      char = charList[i]
+      let nowIndex = lowtitle.indexOf(char, lastIndex + 1)
+      if (nowIndex == -1) {
+        isMatch = false
+        break
+      } else {
+        if (nowIndex != 0) {
+          let prevChar = lowtitle[nowIndex - 1]
+          let alphaRegex = /^[a-z0-9]$/
+          if (alphaRegex.test(prevChar)) matchScore += nowIndex - lastIndex
         }
-
-        if (isMatch) {
-            e.matchScore = matchScore
-            resultList.push(e)
-        }
+        lastIndex = nowIndex
+      }
     }
-    return resultList.sort((a, b) => a.matchScore - b.matchScore)
+
+    if (isMatch) {
+      e.matchScore = matchScore
+      resultList.push(e)
+    }
+  }
+  return resultList.sort((a, b) => a.matchScore - b.matchScore)
 }
 
 function RefreshFeatures() {
-    CacheCheck()
+  CacheCheck()
 
-    let features = utools.getFeatures()
-    features.forEach((value) => {
-        utools.removeFeature(value.code)
-    })
+  let features = utools.getFeatures()
+  features.forEach((value) => {
+    utools.removeFeature(value.code)
+  })
 
-    let cmd_list = []
+  let cmd_list = []
 
-    appListCache.forEach((value) => {
-        cmd_list.push(value.title)
-    })
+  appListCache.forEach((value) => {
+    cmd_list.push(value.title)
+  })
 
-    utools.setFeature({
-        "code": "start_app",
-        "explain": "Steam启动该应用",
-        "cmds": cmd_list,
-        "platform": [
-                "win32"
-            ]
-    })
+  utools.setFeature({
+    code: 'start_app',
+    explain: 'Steam启动该应用',
+    cmds: cmd_list,
+    platform: ['win32'],
+  })
 
-    console.log(utools.getFeatures())
+  console.log(utools.getFeatures())
 }
 
 utools.onPluginReady(() => {
-    CacheCheck()
-    RefreshFeatures()
+  CacheCheck()
+  RefreshFeatures()
 })
 
 window.exports = {
-    "select_apps": {
-        mode: "list",
-        args: {
-            enter: (action, callbackSetList) => {
-                callbackSetList(appListCache)
-            },
-            search: (action, searchWord, callbackSetList) => {
-                callbackSetList(GetFilteredAppsList(searchWord))
-            },
-            select: (action, itemData) => {
-                window.utools.hideMainWindow()
-                let url = itemData.url
-                window.utools.shellOpenExternal(url)
-                window.utools.outPlugin()
-            },
-            placeholder: "搜索应用名"
-        }
+  select_apps: {
+    mode: 'list',
+    args: {
+      enter: (action, callbackSetList) => {
+        callbackSetList(appListCache)
+      },
+      search: (action, searchWord, callbackSetList) => {
+        callbackSetList(GetFilteredAppsList(searchWord))
+      },
+      select: (action, itemData) => {
+        window.utools.hideMainWindow()
+        let url = itemData.url
+        window.utools.shellOpenExternal(url)
+        window.utools.outPlugin()
+      },
+      placeholder: '搜索应用名',
     },
-    "start_app" :{
-        mode: "none",
-        args: {
-            enter: (action) => {
-                window.utools.hideMainWindow()
-                title = action.payload
-                for (let i in appListCache) {
-                    let app = appListCache[i]
-                    if (app.title === title) {
-                        window.utools.shellOpenExternal(app.url)
-                        break
-                    }
-                }
-                window.utools.outPlugin()
-            }
+  },
+  start_app: {
+    mode: 'none',
+    args: {
+      enter: (action) => {
+        window.utools.hideMainWindow()
+        title = action.payload
+        for (let i in appListCache) {
+          let app = appListCache[i]
+          if (app.title === title) {
+            window.utools.shellOpenExternal(app.url)
+            break
+          }
         }
+        window.utools.outPlugin()
+      },
     },
-    "refresh_cache": {
-        mode: "none",
-        args: {
-            enter: () => {
-                window.utools.hideMainWindow()
-                applicationCache = []
-                CacheCheck()
-                RefreshFeatures()
-                utools.showNotification("已刷新应用列表")
-            }
-        }
-    }
+  },
+  refresh_cache: {
+    mode: 'none',
+    args: {
+      enter: () => {
+        window.utools.hideMainWindow()
+        applicationCache = []
+        CacheCheck()
+        RefreshFeatures()
+        utools.showNotification('已刷新应用列表')
+      },
+    },
+  },
 }
